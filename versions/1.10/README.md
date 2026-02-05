a6942d326b76061e35c4efe11b83d216ca7ed3d0
PatchyPhoenix <gtejas774@gmail.com>
2026-02-05 19:30:49 +0530

    Cleaning - Legacy versions - Legacy previews

--- versions/1.10/README.md
+++
@@ -1,7 +0,0 @@
-# <ins>MinimalTab</ins>
-### A Google Chrome extension to replace the default new tab of Google Chrome with a minimalistic tab
-Features:
-- Minimalistic, productive and beautiful
-- Heavily customizable
-- Unique *Shortcut Drawer*
-- Widgets
--- versions/1.10/scripts/inTabSettings.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("settings").addEventListener('click', function() { 
-    chrome.windows.getCurrent().then((result) => {
-        chrome.sidePanel.open({"windowId":result.id}) 
-    });
-    chrome.sidePanel.setOptions({"path":"settings/index.html"})
-})
