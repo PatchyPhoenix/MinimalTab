28447371a3299572877ea8f6c71e1abf639d277f
PatchyPhoenix <gtejas774@gmail.com>
2026-02-06 12:44:03 +0530

    Cleaning - Group 'about' pages (about, license, privacy, terms, whatsnew) - Updated styles in whatsnew.css for better visual consistency.

--- scripts/about.js
+++ scripts/about.js
@@ -2,5 +2,5 @@ setTimeout(()=>{document.body.style.setProperty('opacity','100%');
 document.body.style.setProperty('background-color','#050505');}, 100)
 
 document.getElementById('backBt').addEventListener('click', function() {
-    window.location.href = "settings/index.html"
+    window.location.href = "index.html"
 })
